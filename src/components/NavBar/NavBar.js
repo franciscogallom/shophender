9845d97d72057a5b3760f2b1876c7ac65cff8cf4
franciscogallom <franciscogallomolinuevo@gmail.com>
2020-08-22 01:23:39 -0300

    NavBar improvements

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,58 +7,126 @@ import "./navbar.scss";
 import menuIcon from '../../assets/img/menu.svg'
 
 import CartIcon from '../CartIcon/CartIcon'
+import TypeProduct from "./TypeProduct";
 
 const NavBar = () => {
 
     const isMobileOrTablet = window.screen.width < 1024;
 
-    const [open, setOpen] = useState(!(isMobileOrTablet))
-    // En version mobile y tablet, el valor inicial de open es false. Por lo tanto el menu iniciara cerrado.
-    // En version desktop, open es true, y esto me permite mostrar los items del menu en el NavBar.
+    // En version mobile y tablet, el valor inicial de openMenu es false. Por lo tanto el menu iniciara cerrado.
+    // En version desktop, openMenu es true, y esto me permite mostrar los items del menu en el NavBar.
+    const [openMenu, setOpenMenu] = useState(!(isMobileOrTablet))
+
+    // Manejo las aperturas y cierres de los tipos de productos de Hombre, Mujer, y Ver Todo. En todas las pantallas debe aparecer cerrado
+    const [listCategoriesMen, setListCategoriesMen] = useState(false)
+    const [listCategoriesWomen, setListCategoriesWomen] = useState(false)
+    const [listCategoriesAll, setListCategoriesAll] = useState(false)
+
+    // Al hacer click en el menu, en shopender, o en cart, cierro todos los <TypeComponent /> para que luego al abrir el menu no aparezcan ya abiertas las distintas categorias.
+    const CloseAllListCategories = () => {
+        setListCategoriesMen(false)
+        setListCategoriesWomen(false)
+        setListCategoriesAll(false)
+    }
+
+    // Al hacer click en un tipo de producto, cierro todos lo del NavBar
+    const CloseAllListCategoriesAndMenu = () => {
+        CloseAllListCategories();
+        setOpenMenu(false);
+    }
+
+    // Si es mobile o tablet, cierra todo el menu. Si es desktop, solo cierra los tipos de productos.
+    const closeChoice = () => {
+        return isMobileOrTablet ? CloseAllListCategoriesAndMenu() : CloseAllListCategories()
+    }
 
     return (
-        // En los onClick, setOpen() solo se tiene que ejecutar en version mobile y tablet.
+        // En los onClick, setOpenMenu() solo se tiene que ejecutar en version mobile y tablet.
+        // Para que no me queden abiertos dos o mas <TypeComponent/> a la vez, al hacer click en uno, cierro los otros dos, poniendo el state en false
 
         <nav className='navbar'>
             <ul>
+                
                 <li>
                     <img 
                         className='menu-icon-img'
-                        onClick={() => {isMobileOrTablet && setOpen(!open)}} 
+                        onClick={() => {isMobileOrTablet && 
+                                setOpenMenu(!openMenu)
+                                CloseAllListCategories()}} 
                         src={menuIcon} 
                         alt="Menu"
                     />
                     {   
-                        // Si open es true, muestro las cateogiras de productos. (Funcionalidad para mobile y tablet)
-                        open &&
-                            <ul 
-                                onClick={() => {isMobileOrTablet && setOpen(!open)}} 
-                                className='products-category-items'
-                            >
-                                <li><NavLink to='/products'>HOMBRE</NavLink></li>
-                                <li><NavLink to='/products'>MUJER</NavLink></li>
-                                <li><NavLink to='/products'>VER TODO</NavLink></li>
+                        // Si openMenu es true, muestro las cateogiras de productos. (Funcionalidad para mobile y tablet)
+                        openMenu &&
+                            <ul className='products-gender'>
+
+                                <li 
+                                    onClick={() => {
+                                        setListCategoriesMen(!listCategoriesMen)
+                                        setListCategoriesWomen(false)
+                                        setListCategoriesAll(false)}
+                                    }
+                                    className={`${listCategoriesMen}`}
+                                >
+                                    HOMBRE
+                                </li>
+                                {listCategoriesMen && 
+                                    <TypeProduct closeAll={closeChoice} sex='men' />}
+                                
+                                <li 
+                                    onClick={() => {
+                                        setListCategoriesWomen(!listCategoriesWomen)
+                                        setListCategoriesMen(false)
+                                        setListCategoriesAll(false)}}
+                                        className={`${listCategoriesWomen}`}
+                                >
+                                    MUJER
+                                </li>
+                                {listCategoriesWomen && 
+                                    <TypeProduct closeAll={closeChoice} sex='women' />}
+                                
+                                <li 
+                                    onClick={() => {
+                                        setListCategoriesAll(!listCategoriesAll)
+                                        setListCategoriesMen(false)
+                                        setListCategoriesWomen(false)}}
+                                        className={`${listCategoriesAll}`}
+                                >
+                                    VER TODO
+                                </li>
+                                {listCategoriesAll && 
+                                    <TypeProduct closeAll={closeChoice} sex='all' />}
+
                             </ul>
                     }   
-                </li>
+                </li>                                         
+                
                 <li>
                     <NavLink 
                         to="/" 
                         // No puede abrir el menu, pero si cerrarlo.
-                        onClick={() => {isMobileOrTablet && setOpen(open && false)}}
+                        onClick={() => {isMobileOrTablet && 
+                                setOpenMenu(openMenu && false)
+                                CloseAllListCategories()}}
                     >
                         <h1 className='shophender'>shophender</h1>
                     </NavLink>
                 </li>
+
                 <li>
                     <NavLink 
                         to="/cart"
                         className='cart-icon-img' 
-                        activeClassName="activeLink"
+                        // No puede abrir el menu, pero si cerrarlo.
+                        onClick={() => {isMobileOrTablet && 
+                                setOpenMenu(openMenu && false)
+                                CloseAllListCategories()}}
                     >
                         <CartIcon />
                     </NavLink>
                 </li>
+
             </ul>
         </nav>
     );
---
+++ src/components/NavBar/TypeProduct.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { NavLink } from 'react-router-dom'
+
+import './typeProduct.scss'
+
+const TypeProduct = (props) => {
+    return (
+
+        <ul className='container-type-product'>
+            <li><NavLink onClick={props.closeAll} to={`/products/${props.sex}/tshirts`}>- REMERAS -</NavLink></li>
+            <li><NavLink onClick={props.closeAll} to={`/products/${props.sex}/pants`}>- PANTALONES -</NavLink></li>
+            <li><NavLink onClick={props.closeAll} to={`/products/${props.sex}/coats`}>- ABRIGOS -</NavLink></li>
+            <li><NavLink onClick={props.closeAll} to={`/products/${props.sex}/shorts`}>- SHORTS -</NavLink></li>
+            <li><NavLink onClick={props.closeAll} to={`/products/${props.sex}/shoes`}>- CALZADO -</NavLink></li>
+            <li><NavLink onClick={props.closeAll} to={`/products/${props.sex}/all`}>- TODO -</NavLink></li>
+        </ul>
+    )
+}
+
+export default TypeProduct
