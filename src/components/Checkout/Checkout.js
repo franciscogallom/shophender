f6dd2e86e0adebe5691b777c8d7b49948229c562
franciscogallom <franciscogallomolinuevo@gmail.com>
2020-09-10 01:58:32 -0300

    New components, Checkout and CheckoutForm.

---
+++ src/components/Checkout/Checkout.js
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react'
+
+import CheckoutForm from '../CheckoutForm/CheckoutForm'
+
+import './checkout.scss'
+
+import checkPay from '../../assets/img/check.svg'
+
+import TotalToPayContext from '../../context/TotalToPayProvider'
+import ItemsQuantityContext from '../../context/ItemsQuantityProvider'
+import ButtonCallToAction from '../ButtonCallToAction/ButtonCallToAction'
+
+const Checkout = () => {
+
+    const contextTotalToPay = useContext(TotalToPayContext)
+    const {totalToPay} = contextTotalToPay
+
+    const contextItems = useContext(ItemsQuantityContext)
+    const { itemsQuantity } = contextItems
+
+    return (
+        <section className='checkout margin-t'>
+            <h1>CHECKOUT</h1>
+            {
+                itemsQuantity.map(item => {
+                    return  <article key={item.key}>
+                                <p>{item.nameProduct} x {item.quantity}</p>
+                                <span>${item.priceProduct}</span>
+                            </article>
+                })
+            }
+            <p className='checkout-total'>TOTAL A PAGAR: ${totalToPay}</p>
+            <CheckoutForm />
+            <ButtonCallToAction text='CONFIRMAR COMPRA ' imgBtn={checkPay} alt='Confirmar compra.' />
+        </section>
+    )
+}
+
+export default Checkout;
---
+++ src/components/CheckoutForm/CheckoutForm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './checkoutForm.scss'
+
+const CheckoutForm = () => {
+    return (
+        <form className='checkout-form'> 
+            <input type="text" placeholder='Nombre.' />
+            <input type="text" placeholder='Apellido.'/>
+            <input type="number" placeholder='Número de celular.'/>
+            <input type="email" placeholder='Email.'/>
+            <input type="email" placeholder='Ingrese nuevamente su email.'/>
+        </form>
+    )
+}
+
+export default CheckoutForm;
