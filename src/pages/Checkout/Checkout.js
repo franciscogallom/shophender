5cd6426d3a23cf3d13409e1e7a2f3cfa42c10ddf
franciscogallom <franciscogallomolinuevo@gmail.com>
2020-09-28 21:49:10 -0300

    Storing purchases in Firebase. Logic to show and hide components in Checkout.

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,25 +1,81 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+
+import { getFirestore } from '../../firebase'
+import * as firebase from 'firebase/app'
+import 'firebase/firestore'
 
 import './checkout.scss'
 
 import checkPay from '../../assets/img/check.svg'
 import signIn from '../../assets/img/sign-in.svg'
 
-import CheckoutForm from '../../components/CheckoutForm/CheckoutForm'
 import ButtonCallToAction from '../../components/ButtonCallToAction/ButtonCallToAction'
 import NoMatch from '../NoMatch/NoMatch'
+import Loader from '../../components/Loader/Loader'
 
 import ProductsInCartContext from '../../context/ProductsInCartProvider'
 import AuthContext from '../../context/AuthProvider'
 
 const Checkout = () => {
 
-    const { productsInCart } = useContext(ProductsInCartContext)
+    const { productsInCart, setProductsInCart } = useContext(ProductsInCartContext)
 
     const { email } = useContext(AuthContext)
 
+    const [name, setName] = useState('')
+    const [surname, setSurname] = useState('')
+    const [phone, setPhone] = useState('')
+
+    const [orderID, setOrderID] = useState('')
+    const [buyCompleted, setBuyCompleted] = useState(false)
+
+    const [loader, setLoader] = useState(false)
+
+    const handleBuy = () => {
+        setLoader(true)
+        const db = getFirestore()
+        const totalToPay = productsInCart.reduce((accumulator, currentValue) => accumulator + currentValue.pricePerQuantity, 0)
+
+        const items = productsInCart.map(item => {
+            return({
+                title: item.nameProduct,
+                id: item.id,
+                totalPrice: item.pricePerQuantity,
+                quantity: item.quantity
+            })
+        })
+
+        const orders = db.collection('orders')
+        const newOrder = {
+            buyer: {name: name, surname: surname, phone: phone, email: email},
+            items: items,
+            date: firebase.firestore.Timestamp.fromDate(new Date()),
+            total: totalToPay
+        }
+
+        orders.add(newOrder).then(({ id }) => {
+            setOrderID(id)
+        }).catch(err => {
+            console.log('error ' + err)
+        }).finally(() => {
+            setLoader(false)
+            setBuyCompleted(true)
+            setProductsInCart([])
+        })
+    }
+
     return (
-        productsInCart[0]
+        loader ? <Loader/> :
+        // Si la compra esta completada, doy feedback del numero de orden y demas. Si no es así, trabajo con el checkout.
+        
+        buyCompleted ?                         
+            <div className='margin-t buy-completed'>
+                <h1>{name}, gracias por tu compra.</h1>
+                <p>Código de la orden: <strong>{orderID}</strong></p>
+            </div> 
+            :
+        
+            productsInCart[0]
             ?
                 <section className='checkout margin-t'>
                     <h1>CHECKOUT</h1>
@@ -44,12 +100,13 @@ const Checkout = () => {
                                 </div>
                             :
                         <>
-                            <CheckoutForm />
-                            <ButtonCallToAction 
-                                link = '/'
-                                text = 'CONFIRMAR COMPRA ' 
-                                imgBtn = {checkPay} 
-                            />
+                            <p className='p-checkout-form'>Estas comprando como {email}</p>
+                            <form className='checkout-form'> 
+                                <input type="text" placeholder='Nombre.' onChange = {(e) => setName(e.target.value)} />
+                                <input type="text" placeholder='Apellido.' onChange = {(e) => setSurname(e.target.value)} />
+                                <input type="number" placeholder='Número de celular.' onChange = {(e) => setPhone(e.target.value)} />
+                            </form>
+                            {(phone.length > 5 && name.length > 1 && surname.length > 1) && <button onClick={handleBuy} className='confirm-buy-btn'>CONFIRMAR COMPRA <img src={checkPay} alt="$"/></button>}
                         </>
                     }
                 </section>
