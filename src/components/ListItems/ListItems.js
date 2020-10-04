1f5d4e6c489343100528f771d1ee4ae62ceb1c20
franciscogallom <franciscogallomolinuevo@gmail.com>
2020-10-03 23:31:28 -0300

    Loader on Home.

--- src/components/ListItems/ListItems.js
+++ src/components/ListItems/ListItems.js
@@ -1,44 +1,12 @@
-import React, { useState, useEffect } from 'react'
-
-import  { Redirect } from 'react-router-dom'
-
-import { getFirestore } from '../../firebase'
+import React from 'react'
 
 import'./listItems.scss';
 
 import ItemList from '../ItemList/ItemList'
 
-const ListItem = (props) => {
-
-    const [items, setItems] = useState({})
-
-    const [err, setErr] = useState(false)
-
-    useEffect(() => {
-        const db = getFirestore()
-        const itemCollection = db.collection("items")
-        let filter = itemCollection.where('sex', '==', `${props.filterSex}`).limit(4)
-        const notFilter = itemCollection.limit(4)
-
-        if (props.filterSex === null) filter = notFilter
-
-        filter.get().then((querySnapshot) => {
-            if(querySnapshot.size === 0) {
-                console.log('querySnapshot.size === 0.')
-                setErr(true)
-                return
-            }
-            setItems(querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })))
-        })
-        .catch((error) => {
-            console.log('Error to find the item. Error: ', error)
-            setErr(true)
-        })
-        // eslint-disable-next-line 
-    }, [])
+const ListItem = ({ items }) => {
 
     return (
-        err ? <Redirect to = '/404' /> :
         Object.keys(items).length !== 0 &&
         <section className='container-list-items'>
             {
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,19 +1,95 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { Redirect } from 'react-router-dom'
 
 import './home.scss'
 
+import { getFirestore } from '../../firebase'
+
 import HomeBanner from '../../components/HomeBanner/HomeBanner'
 import ListItems from '../../components/ListItems/ListItems'
+import Loader from '../../components/Loader/Loader'
 
 const Home = () => {
 
+    const [itemsMen, setItemsMen] = useState({})
+    const [itemsWomen, setItemsWomen] = useState({})
+    const [itemsAll, setItemsAll] = useState({})
+
+    const [err, setErr] = useState(false)
+
+    const [loader, setLoader] = useState(false)
+
+    useEffect(() => {
+        setLoader(true)
+        const db = getFirestore()
+        const itemCollection = db.collection("items")
+        let itemsMen = itemCollection.where('sex', '==', 'men').limit(4)
+        let itemsWomen = itemCollection.where('sex', '==', 'women').limit(4)
+        let itemsAll = itemCollection.limit(4)
+        
+        // Obtengo cuatro productos de hombre.
+        itemsMen.get().then((querySnapshot) => {
+            if(querySnapshot.size === 0) {
+                console.log('querySnapshot.size === 0.')
+                setErr(true)
+                return
+            }
+            setItemsMen(querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })))
+        })
+        .catch((error) => {
+            console.log('Error to find the item. Error: ', error)
+            setErr(true)
+        })
+        .finally(() => {
+            setLoader(false)
+        })
+
+        // Obtengo cuatro productos de mujer.
+        itemsWomen.get().then((querySnapshot) => {
+            if(querySnapshot.size === 0) {
+                console.log('querySnapshot.size === 0.')
+                setErr(true)
+                return
+            }
+            setItemsWomen(querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })))
+        })
+        .catch((error) => {
+            console.log('Error to find the item. Error: ', error)
+            setErr(true)
+        })
+        .finally(() => {
+            setLoader(false)
+        })
+
+        // Obtengo cuatro aleatorios.
+        itemsAll.get().then((querySnapshot) => {
+            if(querySnapshot.size === 0) {
+                console.log('querySnapshot.size === 0.')
+                setErr(true)
+                return
+            }
+            setItemsAll(querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })))
+        })
+        .catch((error) => {
+            console.log('Error to find the item. Error: ', error)
+            setErr(true)
+        })
+        .finally(() => {
+            setLoader(false)
+        })
+        // eslint-disable-next-line 
+    }, [])
+
     return (
+        err ? <Redirect to = '/404' /> :
+        loader ? <Loader /> :
         <section className='home-container margin-t'>
             <header>
                 <h1>shophender</h1>
             </header>
             
-            <ListItems filterSex = 'men' />
+            <ListItems items = {itemsMen} />
 
             <HomeBanner  
                 bannerText = 'hombres' 
@@ -21,7 +97,7 @@ const Home = () => {
                 bannerGender = 'men'
             />
 
-            <ListItems filterSex = 'women' />
+            <ListItems items = {itemsWomen} />
 
             <HomeBanner  
                 bannerText = 'mujeres' 
@@ -29,7 +105,7 @@ const Home = () => {
                 bannerGender = 'women'
             />
 
-            <ListItems filterSex = {null} />
+            <ListItems items = {itemsAll} />
 
             <HomeBanner  
                 bannerText = 'ver.todo' 
