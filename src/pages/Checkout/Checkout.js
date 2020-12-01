174c0378998da987c6a89d9a8715713a124c6960
franciscogallom <franciscogallomolinuevo@gmail.com>
2020-12-01 17:37:08 -0300

    Formik validation.

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './checkout.scss'
 
 import { useCheckout } from './useCheckout'
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 import checkPay from '../../assets/img/check.svg'
 
@@ -48,15 +48,34 @@ const Checkout = () => {
                             <Formik
                                 initialValues = {{name: '', surname: '', address: '', phone: '', city: ''}}
                                 onSubmit = {(values) => handleBuy(values)}
+                                validate = {values => {
+                                    const errors = {}
+                                    if (!values.name) errors.name = 'Campo requerido.'
+                                    if (!values.surname) errors.surname = 'Campo requerido.'
+                                    if (!values.city) errors.city = 'Campo requerido.'
+                                    if (!values.address) errors.address = 'Campo requerido.'
+                                    if (!values.phone) { 
+                                        errors.phone = 'Campo requerido.'
+                                    } else if (!/[0-9]{6,20}/.test(values.phone)){
+                                        errors.phone = 'Numero inválido.';
+                                    }
+                                    return errors
+                                }
+                            }
                             >
                                 {
-                                    () =>   
+                                    (errors) =>   
                                         <Form className = 'checkout-form' > 
                                             <Field name = "name" placeholder = 'Nombre.' />
+                                            <ErrorMessage name = 'name' component = 'p' className = 'p-error' />
                                             <Field name = "surname" placeholder = 'Apellido.' />
+                                            <ErrorMessage name = 'surname' component = 'p' className = 'p-error' />
                                             <Field name = "city" placeholder = 'Ciudad.' />
+                                            <ErrorMessage name = 'city' component = 'p' className = 'p-error' />
                                             <Field name = "address" placeholder = 'Direccion.' />
+                                            <ErrorMessage name = 'address' component = 'p' className = 'p-error' />
                                             <Field name = "phone" placeholder = 'Número de celular.' />
+                                            <ErrorMessage name = 'phone' component = 'p' className = 'p-error' />
                                             <button 
                                                 className = 'confirm-buy-btn'>
                                                     CONFIRMAR COMPRA 
