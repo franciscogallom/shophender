6e7a68954b4b85cdca0a13a6b94898bb7cc1955a
franciscogallom <franciscogallomolinuevo@gmail.com>
2020-12-14 23:18:16 -0300

    useLocalStorage Custom Hook to handle the cart.

--- src/pages/ItemDetail/useItemsForItemDetail.js
+++ src/pages/ItemDetail/useItemsForItemDetail.js
@@ -4,6 +4,8 @@ import { useParams } from 'react-router-dom'
 
 import { getItemsForItemDetail } from '../../firebase'
 
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+
 import ProductsInCartContext from '../../context/ProductsInCartProvider'
 
 export function useItemsForItemDetail(){
@@ -14,7 +16,9 @@ export function useItemsForItemDetail(){
     const [items, setItems] = useState({})
     const [err, setErr] = useState(false)
 
-    const { productsInCart, setProductsInCart } = useContext(ProductsInCartContext)
+    const [ productsInCart, setProductsInCart ] = useLocalStorage('products', [])
+
+    // const [ productsInLocalStorage, setProductsInLocalStorage ] = useLocalStorage('products', productsInCart)
 
     const { id } = useParams()
 
@@ -26,14 +30,25 @@ export function useItemsForItemDetail(){
         let isInTheCart = false;
         productsInCart.forEach(product => {
             if (product.id === item.id) {
-                product.quantity++
-                product.pricePerQuantity += product.unitPrice
-                setProductsInCart(prevItems => [...prevItems])
+                // Elimino el producto y lo vuelvo a añadir actualizado.
+                const newQuantity = product.quantity + 1
+                const newpricePerQuantity = product.pricePerQuantity + product.unitPrice
+                const result = productsInCart.filter(item => item.id !== id)
+                setProductsInCart([...result, {
+                    nameProduct: item.nameProduct, 
+                    imgProduct: item.img1,
+                    imgProduct2: item.img2, 
+                    pricePerQuantity: newpricePerQuantity,
+                    unitPrice: item.unitPrice,
+                    key: item.id,
+                    id: item.id,
+                    quantity: newQuantity
+                }])    
                 isInTheCart = true;
             }
         })
-        !isInTheCart && 
-            setProductsInCart((prevItems) => [...prevItems, {
+        !isInTheCart &&
+            setProductsInCart([...productsInCart, {
                 nameProduct: item.nameProduct, 
                 imgProduct: item.img1,
                 imgProduct2: item.img2, 
@@ -42,7 +57,9 @@ export function useItemsForItemDetail(){
                 key: item.id,
                 id: item.id,
                 quantity: 1
-            }])
+            }])    
+        // console.log(productsInCart)
+        // setProductsInLocalStorage(productsInCart)
     }
 
     return { err, loader, item, items, setProductsInCartFunction }
